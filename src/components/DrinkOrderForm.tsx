b261fb035345df84d5312ec9a4a87f89c0d223de
ayakamada <sdkamada@gmail.com>
2023-07-22 12:58:56 +0900

    下に余白があったので、styleを調整

--- src/components/DrinkOrderForm.tsx
+++ src/components/DrinkOrderForm.tsx
@@ -10,7 +10,7 @@ const DrinkOrderForm = () => {
 
   return (
     <div className="DrinkOrderForm">
-      <div className="bg-gray-100 pt-20">
+      <div className="bg-gray-100 py-20 min-h-screen">
         <h1 className="mb-10 text-center text-2xl font-bold">Drink Order Form</h1>
         <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
