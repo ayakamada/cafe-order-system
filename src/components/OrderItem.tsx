3006ae0ee0db4abf4fa7d97fb0d48ebbe684c2e4
ayakamada <sdkamada@gmail.com>
2023-07-22 12:21:04 +0900

    カウント用関数を配置

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -1,11 +1,18 @@
 import { IDrink } from "../types/drinks";
 
-const OrderItem = ({ item }: { item: IDrink }) => {
+interface OrderItemProps {
+  item: IDrink;
+  count: number; // クリック回数を受け取る
+  onIncreaseClick: () => void; // プラスボタン用
+  onDecreaseClick: () => void; // マイナスボタン用
+}
+
+const OrderItem = ({ item, count, onIncreaseClick, onDecreaseClick }: OrderItemProps) => {
   return (
     <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
       <div className="relative">
         <span className="absolute -top-3 left-0 px-3 py-2 text-xs font-bold leading-none text-red-100 transform bg-blue-600 rounded-full">
-          0
+          {count}
         </span>
         <img src={item.img} alt="product" className="w-full rounded-lg sm:w-40" />
       </div>
@@ -16,12 +23,18 @@ const OrderItem = ({ item }: { item: IDrink }) => {
         </div>
         <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
           <div className="flex items-center border-gray-100">
-            <button className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+            <button
+              className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
+              onClick={onDecreaseClick}
+            >
               {" "}
               -{" "}
             </button>
 
-            <button className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+            <button
+              className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
+              onClick={onIncreaseClick}
+            >
               {" "}
               +{" "}
             </button>
