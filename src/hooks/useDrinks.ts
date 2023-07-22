b76b6aad8cd70480d492336755497fcb2f546aa0
ayakamada <sdkamada@gmail.com>
2023-07-22 12:20:03 +0900

    カウント用handleClickを作成

--- src/hooks/useDrinks.ts
+++ src/hooks/useDrinks.ts
@@ -2,15 +2,40 @@ import { useRecoilValue, useRecoilState } from "recoil";
 import { useCallback } from "react";
 
 import drinksState from "../recoil/atoms/order";
+import clickCountState from "../recoil/atoms/order-count";
 
+import { IDrink } from "../types/drinks";
 
 const useTodos = () => {
   const [drinks, setDrinks] = useRecoilState(drinksState);
+  const clickCountValue = useRecoilValue(clickCountState);
+  const [clickCounts, setClickCounts] = useRecoilState(clickCountState);
 
+  const handleItemClick = (id: string, isIncrease: boolean) => {
+    const newClickCounts = [...clickCounts];
+    const index = drinks.findIndex((drink: IDrink) => drink.id === id);
+
+    if (index !== -1) {
+      const countKey = `${id}-count`;
+      if (isIncrease) {
+        newClickCounts[index] = {
+          ...newClickCounts[index],
+          [countKey]: newClickCounts[index][countKey] + 1,
+        };
+      } else {
+        newClickCounts[index] = {
+          ...newClickCounts[index],
+          [countKey]: Math.max(0, newClickCounts[index][countKey] - 1),
+        };
+      }
+      setClickCounts(newClickCounts);
+    }
+  };
 
   return {
     drinks,
-    setDrinks,
+    clickCountValue,
+    handleItemClick,
   };
 };
 
