293591f36511a2fe5317b385a193229e70d872fa
ayakamada <sdkamada@gmail.com>
2023-07-22 11:17:17 +0900

    商品データを作成

---
+++ src/data/drinks.ts
@@ -0,0 +1,36 @@
+import { IDrink } from "../types/drinks";
+
+const Drinks: IDrink[] = [
+  {
+    id: "coffee",
+    name: "コーヒー",
+    price: 480,
+    img: "https://dummyimage.com/600x400/000/fff",
+  },
+  {
+    id: "tea",
+    name: "紅茶",
+    price: 280,
+    img: "https://dummyimage.com/600x400/000/fff",
+  },
+  {
+    id: "milk",
+    name: "ミルク",
+    price: 180,
+    img: "https://dummyimage.com/600x400/000/fff",
+  },
+  {
+    id: "coke",
+    name: "コーラ",
+    price: 190,
+    img: "https://dummyimage.com/600x400/000/fff",
+  },
+  {
+    id: "beer",
+    name: "ビール",
+    price: 580,
+    img: "https://dummyimage.com/600x400/000/fff",
+  },
+];
+
+export default Drinks;
