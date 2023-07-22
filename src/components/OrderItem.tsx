4b0d6fb4d3ab28af585a3adf21108a1e3895c73e
ayakamada <sdkamada@gmail.com>
2023-07-22 15:35:48 +0900

    style改善

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -1,3 +1,4 @@
+import React, { useCallback } from "react";
 import { IDrink } from "../types/drinks";
 
 interface OrderItemProps {
@@ -7,13 +8,13 @@ interface OrderItemProps {
   onDecreaseClick: () => void; // マイナスボタン用
 }
 
-const OrderItem = ({ item, count, onIncreaseClick, onDecreaseClick }: OrderItemProps) => {
+const OrderItem = React.memo(({ item, count, onIncreaseClick, onDecreaseClick }: OrderItemProps) => {
   return (
     <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
       <div className="relative">
         <div
           id={`${item.id}-count`}
-          className="absolute -top-3 right-0 px-3 py-2 text-xs font-bold leading-none text-red-100 transform bg-white border border-zinc-300 rounded-full text-zinc-800"
+          className="absolute -top-3 right-0 px-3 py-2 sm:text-xs text-sm font-bold leading-none transform bg-white border border-zinc-300 rounded-full text-blue-600"
         >
           {count}
         </div>
@@ -22,21 +23,20 @@ const OrderItem = ({ item, count, onIncreaseClick, onDecreaseClick }: OrderItemP
       <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
         <div className="mt-5 sm:mt-0">
           <h2 className="text-lg font-bold text-gray-900">{item.name}</h2>
-          <p className="mt-1 text-left text-md text-gray-700">¥{item.price}</p>
+          <p className="mt-1 sm:text-left text-md text-gray-700">¥{item.price}</p>
         </div>
-        <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-          <div id={item.id} className="flex items-center border-gray-100">
+        <div className="mt-4 flex justify-end sm:justify-start sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+          <div id={item.id} className="flex border-gray-100 h-full">
             <button
               id={`${item.id}-up`}
-              className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
+              className="w-20 h-1/2 block cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base"
               onClick={onDecreaseClick}
             >
               <span>-</span>
             </button>
-
             <button
               id={`${item.id}-down`}
-              className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
+              className="w-20 h-1/2 block cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base"
               onClick={onIncreaseClick}
             >
               <span>+</span>
@@ -46,5 +46,5 @@ const OrderItem = ({ item, count, onIncreaseClick, onDecreaseClick }: OrderItemP
       </div>
     </div>
   );
-};
+});
 export default OrderItem;
