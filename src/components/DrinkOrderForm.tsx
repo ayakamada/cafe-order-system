f9e921a0c8de0a981c41edca72e2d1e1ab63d103
ayakamada <sdkamada@gmail.com>
2023-07-23 19:35:17 +0900

    料金表示方法修正

--- src/components/DrinkOrderForm.tsx
+++ src/components/DrinkOrderForm.tsx
@@ -13,7 +13,7 @@ const DrinkOrderForm = () => {
   return (
     <div className="DrinkOrderForm">
       <div className="">
-        <h1 className="mb-10 text-center text-2xl font-bold">Drink Order Form</h1>
+        <h1 className="mb-10 text-center text-2xl font-bold">Track Cafe Drink Order</h1>
         <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
             {drinks.map((item: IDrink, index) => {
