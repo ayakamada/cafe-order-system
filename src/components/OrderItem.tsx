dcfbb961dad41642fbbd7053c5b31294787821af
ayakamada <sdkamada@gmail.com>
2023-07-23 13:08:48 +0900

    クリックアニメーション用のclass toggle関数を追加

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { IDrink } from "../types/drinks";
 
 interface OrderItemProps {
@@ -9,16 +9,30 @@ interface OrderItemProps {
 }
 
 const OrderItem = React.memo(({ item, count, onIncreaseClick, onDecreaseClick }: OrderItemProps) => {
+  const [animate, setAnimate] = useState(false);
+
+  /*
+  クリックアニメーション用のclass toggle関数
+  */
+  const classToggle = () => {
+    setAnimate(true);
+    setTimeout(() => {
+      setAnimate(false);
+    }, 500);
+  };
+
   return (
     <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-      <div className="relative">
+      <div className="relative ">
         <div
           id={`${item.id}-count`}
-          className="absolute -top-3 right-0 px-3 py-2 sm:text-xs text-sm font-bold leading-none transform bg-white border border-zinc-300 rounded-full text-blue-600"
+          className={`absolute -top-3 right-0 px-3 py-2 sm:text-xs text-sm font-bold leading-none transform bg-white border border-zinc-300 rounded-full text-blue-600 ${
+            animate ? "animate-wobble-hor-bottom" : ""
+          }`}
         >
           {count}
         </div>
-        <img src={item.img} alt="product" className="w-full rounded-lg sm:w-40" />
+        <img src={item.img} alt="product" className="w-full rounded-lg sm:w-40 object-cover sm:h-28 h-48" />
       </div>
       <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
         <div className="mt-5 sm:mt-0">
@@ -29,15 +43,21 @@ const OrderItem = React.memo(({ item, count, onIncreaseClick, onDecreaseClick }:
           <div id={item.id} className="flex border-gray-100 h-full">
             <button
               id={`${item.id}-up`}
-              className="w-20 h-1/2 block cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base"
-              onClick={onDecreaseClick}
+              className={`w-20 h-1/2 block cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base`}
+              onClick={() => {
+                onDecreaseClick();
+                classToggle();
+              }}
             >
               <span>-</span>
             </button>
             <button
               id={`${item.id}-down`}
-              className="w-20 h-1/2 block cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base"
-              onClick={onIncreaseClick}
+              className={`w-20 h-1/2 block cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base`}
+              onClick={() => {
+                onIncreaseClick();
+                classToggle();
+              }}
             >
               <span>+</span>
             </button>
