ddfa823df3b67f2fd99a6119533e0bae2626d51c
ayakamada <sdkamada@gmail.com>
2023-07-23 22:11:46 +0900

    idで管理に変更

--- src/data/drinks.ts
+++ src/data/drinks.ts
@@ -2,31 +2,36 @@ import { IDrink } from "../types/drinks";
 
 const Drinks: IDrink[] = [
   {
-    id: "coffee",
+    id: 1,
+    code: "coffee",
     name: "コーヒー",
     price: 480,
     img: "https://source.unsplash.com/nzyzAUsbV0M",
   },
   {
-    id: "tea",
+    id: 2,
+    code: "tea",
     name: "紅茶",
     price: 280,
     img: "https://source.unsplash.com/oBoELXgJR1w",
   },
   {
-    id: "milk",
+    id: 3,
+    code: "milk",
     name: "ミルク",
     price: 180,
     img: "https://source.unsplash.com/5SN5N5-JM3c",
   },
   {
-    id: "coke",
+    id: 4,
+    code: "coke",
     name: "コーラ",
     price: 190,
     img: "https://source.unsplash.com/D8XcEUt3Tmc",
   },
   {
-    id: "beer",
+    id: 5,
+    code: "beer",
     name: "ビール",
     price: 580,
     img: "https://source.unsplash.com/K8nr6rNDtUE",
