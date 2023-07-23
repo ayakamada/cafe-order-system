54db56195ffecc706f206677bb0eb3fd04f374a6
ayakamada <sdkamada@gmail.com>
2023-07-23 22:19:11 +0900

    codeに変更

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -30,7 +30,7 @@ const OrderItem = React.memo(({ item, count, onIncreaseClick, onDecreaseClick }:
     <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
       <div className="relative ">
         <div
-          id={`${item.id}-count`}
+          id={`${item.code}-count`}
           className={`absolute -top-3 right-0 px-3 py-2 sm:text-xs text-sm font-bold leading-none transform bg-white border border-zinc-300 rounded-full text-blue-600 ${
             animate ? "animate-wobble-hor-bottom" : ""
           }`}
@@ -45,9 +45,9 @@ const OrderItem = React.memo(({ item, count, onIncreaseClick, onDecreaseClick }:
           <p className="mt-1 sm:text-left text-md text-gray-700">¥{item.price}</p>
         </div>
         <div className="mt-4 flex justify-end sm:justify-start sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-          <div id={item.id} className="flex border-gray-100 h-full">
+          <div id={item.code} className="flex border-gray-100 h-full">
             <button
-              id={`${item.id}-up`}
+              id={`${item.code}-up`}
               className={`w-20 h-1/2 block cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base`}
               onClick={() => {
                 onDecreaseClick();
@@ -56,7 +56,7 @@ const OrderItem = React.memo(({ item, count, onIncreaseClick, onDecreaseClick }:
               <span>-</span>
             </button>
             <button
-              id={`${item.id}-down`}
+              id={`${item.code}-down`}
               className={`w-20 h-1/2 block cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50 text-base`}
               onClick={() => {
                 onIncreaseClick();
