0a075f8a720527be09f72239a83bb48014830c5e
ayakamada <sdkamada@gmail.com>
2023-07-23 22:21:25 +0900

    リファクタリング

--- src/hooks/useDrinks.ts
+++ src/hooks/useDrinks.ts
@@ -1,51 +1,31 @@
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState, useRecoilState } from "recoil";
 import { useCallback } from "react";
 
 import drinksState from "../recoil/atoms/drink";
 import drinkCountState from "../recoil/atoms/drink-count";
 import { totalClickCountState, itemTotalPriceState } from "../recoil/selectors/drink-count";
 
-import { IDrink } from "../types/drinks";
-
 const useDrinks = () => {
   const drinks = useRecoilValue(drinksState);
-  const clickCountValue = useRecoilValue(drinkCountState);
-  const setClickCounts = useSetRecoilState(drinkCountState);
+  const [drinkCounts, setDrinkCounts] = useRecoilState(drinkCountState);
 
   const totalClickCount = useRecoilValue(totalClickCountState);
   const itemTotalPrices = useRecoilValue(itemTotalPriceState);
 
-  // useCallbackを使って、関数をメモ化
-
-  const handleItemClick = useCallback(
-    (id: string, isIncrease: boolean) => {
-      const newClickCounts = [...clickCountValue];
-      const index = drinks.findIndex((drink: IDrink) => drink.id === id);
-
-      if (index !== -1) {
-        // マイナス値にならないように設定
-        const countKey = `${id}-count`;
-        if (isIncrease) {
-          newClickCounts[index] = {
-            ...newClickCounts[index],
-            [countKey]: newClickCounts[index][countKey] + 1,
-          };
-        } else {
-          newClickCounts[index] = {
-            ...newClickCounts[index],
-            [countKey]: Math.max(0, newClickCounts[index][countKey] - 1),
-          };
-        }
-
-        setClickCounts(newClickCounts);
-      }
-    },
-    [drinks, clickCountValue, setClickCounts]
-  );
+  // useCallbackを使うことで、関数の再生成を防ぐ
+  const handleItemClick = (id: number, isIncrease: boolean) => {
+    if (id !== -1) {
+      // マイナス値にならないように設定
+      setDrinkCounts((prevCounts) => ({
+        ...prevCounts,
+        [id]: isIncrease ? prevCounts[id] + 1 : Math.max(0, prevCounts[id] - 1),
+      }));
+    }
+  };
 
   return {
     drinks,
-    clickCountValue,
+    drinkCounts,
     handleItemClick,
     totalClickCount,
     itemTotalPrices,
