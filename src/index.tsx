dd086eefe001df01d4038a65df7bf21867dd787b
ayakamada <sdkamada@gmail.com>
2023-07-26 20:30:17 +0900

    drawer用に追加

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { RecoilRoot } from "recoil";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import "preline";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/preline/dist/*.js,"],
   theme: {
     extend: {
       animation: {
@@ -31,5 +31,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 };
